5c7cb73500dc2dca4d9b793fb57fa7f282cc60dc
yuan.xiong <yuan.xiong@intel.com>
2021-03-24 09:53:48 +0800

    Test create cv::UMat setTo and GPU kernel clfinish in VTune.
    Signed-off-by: yuan.xiong <yuan.xiong@intel.com>

--- src/test_gpu/hello_world.cl
+++ src/test_gpu/hello_world.cl
@@ -1,3 +1,13 @@
-__kernel void hello_world() {
+__kernel void hello_world(const int input,
+						  const __global float * restrict umat) {
+	size_t gid0 = get_global_id(0);
+	size_t gid1 = get_global_id(1);
 
+	size_t gpid0 = get_group_id(0);
+	size_t gpid1 = get_group_id(1);
+
+	size_t lid0 = get_local_id(0);
+	size_t lid1 = get_local_id(1);
+
+	printf("[kernel (%2d,%2d)] group(%2d,%2d), local(%2d,%2d), hello_world\n", gid0,gid1, gpid0,gpid1, lid0,lid1);
 }
--- src/test_gpu/main.cpp
+++ src/test_gpu/main.cpp
@@ -14,6 +14,39 @@ void read_opencl_file() {
     } else {
         std::cout << "Succeeded to load OpenCL file:\t" << file << std::endl;
 	}
+    int input = 100;
+
+	//cv::UMat umat(512, 512, CV_32FC3, cv::Scalar(1.0f));
+	cv::UMat umat(360, 480, CV_32FC3, cv::Scalar(1.0f));
+
+	// no cv setTo
+	//cv::UMat umat(360, 480, CV_32FC3);
+
+#if 0
+    std::string source((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+    cv::ocl::ProgramSource programSource(source.c_str());
+
+    cv::ocl::Program program;
+    std::string buildOption = cv::format("-D TIMES=%d", 1000);
+    std::string errmsg;
+    program.create(programSource, buildOption, errmsg);
+
+	cv::ocl::Kernel kernel("hello_world" ,program);
+    kernel.set(0, input);
+    kernel.set(1, umat);
+
+    //size_t global_size[] = {8, 8};
+    //size_t local_size[]  = {4, 2};
+    size_t global_size[] = {64, 64};
+    size_t local_size[]  = {8, 16};
+    bool success = kernel.run(2, global_size, local_size, true);
+    //bool success = kernel.run(2, global_size, local_size, false);
+    if(success) {
+        std::cout << "Running OpenCL kernel successfully." << std::endl;
+    } else {
+        std::cout << "Running OpenCL kernel failed." << std::endl;
+    }
+#endif
 }
 
 void test_opencv_opencl() {
@@ -23,7 +56,7 @@ void test_opencv_opencl() {
 }
 
 int main() {
-	for(int i=0; i<100*1000; ++i)
+	//for(int i=0; i<100*1000; ++i)
 		test_opencv_opencl();
 
 	return 0;
